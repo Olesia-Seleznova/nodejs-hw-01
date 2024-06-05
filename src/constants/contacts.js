c3e615224e13d4fd52064a606cb33cdd02163efc
Olesia-Seleznova <106627666+Olesia-Seleznova@users.noreply.github.com>
2024-06-05 22:54:42 +0300

    add generateContacts

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,5 @@
-export const PATH_DB =
+import path from 'node:path';
+// import fs from 'node:fs/promises';
+
+const pathToWorkDir = path.join(process.cwd());
+export const PATH_DB = path.join(pathToWorkDir, 'src', 'db', 'db.json');
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,22 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  try {
+    //Читання з існуючогофайлу
+    const readContacts = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(readContacts);
+    console.log('Вміст файлу:', readContacts);
+    //Створення масиву нових контактів
+    const newContacts = Array.from({ length: number }, createFakeContact);
+    //Додавання нових контактів в існуючий масив
+    const updateContacts = [...contacts, ...newContacts];
+    //Запис масиву нових контактів в файл
+    await fs.writeFile(PATH_DB, JSON.stringify(updateContacts, null, 2));
+  } catch (error) {
+    console.error('Помилка читання файлу:', error);
+  }
+};
 
 await generateContacts(5);
